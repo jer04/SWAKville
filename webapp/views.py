16b3350722a6394d52b9ff18907a7f9ad90da702
RockGFY <33272289+RockGFY@users.noreply.github.com>
2019-07-24 16:07:59 -0700

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -25,8 +25,6 @@ def signup(request):
         newblog = Blog(blogTitle=tempblogtitle, user=newuser)
         newblog.save()
 
-        #newUserBlog = UserBlog(author=newuser, blog=newblog)
-        #newUserBlog.save()
         return render(request, 'web/base.html')
         
     else:
@@ -37,23 +35,16 @@ def login(request):
         loginname = request.POST.get('loginname')      
         loginpassword = request.POST.get('loginpassword')
         try:
-            #loginuser = User.objects.filter(username=loginname)
-            tempuser = User.objects.get(username=loginname)
-            #print(tempname)           
+            tempuser = User.objects.get(username=loginname)        
             if tempuser.password == loginpassword:                                              
                 loginblog = Blog.objects.get(user_id=tempuser.id)
-                #print(tempname.id)
-                #print(loginblogID)
-                #tempuser = loginblog.user
-                #print(tempuser.username)
-                #print(tempuser.firstName)
-                #logintitile = loginblog.blogTitle
+
                 request.session['logged_user_id'] = tempuser.id
-                request.session['logged_blog_title'] = loginblog.blogTitle
+
                 posts = Post.objects.filter(user_id=tempuser.id).order_by('created_on')
                 return render(request, 'web/blog_page.html', 
-                {'blogtitle':loginblog.blogTitle,
-                 'loginname':tempuser.firstName, 
+                {'blogtitle':tempuser.blog.blogTitle,
+                 'loginname':tempuser.username, 
                  'posts':posts})
             else:
                 return HttpResponse("wrong password!", content_type='text/plain')
@@ -63,15 +54,10 @@ def login(request):
     else:
         return render(request, 'web/login.html')
 
-def home(request):
-    return render(request, "web/home.html")
-
 def delete(request):
     if request.method == 'POST':
-        name = request.POST.get('loginname')
-        #print(loginname)       
+        name = request.POST.get('loginname')     
         password = request.POST.get('loginpassword')
-        #print(loginpassword)
         try:
             tempname = User.objects.get(username=name)
             print(tempname)
@@ -81,26 +67,20 @@ def delete(request):
             else:
                 return HttpResponse("wrong password!", content_type='text/plain')
         except User.DoesNotExist:
-            return HttpResponse("User not exist.", content_type='text/plain')     
-    
+            return HttpResponse("User not exist.", content_type='text/plain')         
     else:
         return render(request, 'web/delete.html')
 
-def blog_page(request):
-    
+def blog_page(request):  
     current_user_id = request.session.get('logged_user_id')
     current_user = User.objects.get(id=current_user_id)
-    current_blog_title = request.session.get('logged_blog_title')
-    #print(current_user_id)
+
     posts = Post.objects.filter(user_id=current_user_id).order_by('created_on')
     
-    #posts = Post.objects.all().order_by('created_on') # grabs all records from the posts database table.
     return render(request, 'web/blog_page.html', 
-    {'blogtitle':current_blog_title,
-     'loginname':current_user.firstName,
+    {'blogtitle':current_user.blog.blogTitle,
+     'loginname':current_user.username,
      'posts':posts})
-
-# This posts all the posts in order by date created on the blog page.
 
 def post_page(request):
     if request.method == 'POST':
@@ -108,7 +88,6 @@ def post_page(request):
         if form.is_valid():
             current_user_id = request.session.get('logged_user_id')
             current_user = User.objects.get(id=current_user_id)           
-            current_blog_title = request.session.get('logged_blog_title')
             
             title = form.cleaned_data['new_title']
             body = form.cleaned_data['new_body']
@@ -118,20 +97,15 @@ def post_page(request):
             posts = Post.objects.filter(user_id=current_user_id).order_by('created_on')
 
             return render(request, 'web/blog_page.html', 
-            {'blogtitle':current_blog_title,
-            'loginname':current_user.firstName,
+            {'blogtitle':current_user.blog.blogTitle,
+            'loginname':current_user.username,
             'posts':posts})
     else:
-        #form = NewPost()
-        #return render(request, 'blog/post_page.html', {'form': form})
         return render(request, 'web/post_page.html')
-    # return render(request, 'posts/post_page.html')
 
-    
 def post_view(request, post_id):
     post = Post.objects.get(id = post_id)
     context = {
         'post' : post
     }
-    return render(request, 'blog/post_view.html', context)
-
+    return render(request, 'web/postview.html', context)
