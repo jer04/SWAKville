4b2dbda4ceea11360d4702c63f00e679b3fc7c78
RockGFY <33272289+RockGFY@users.noreply.github.com>
2019-07-24 16:07:39 -0700

    Update urls.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,13 +6,14 @@ app_name = 'webapp'
 
 urlpatterns = [
     url('', views.base, name='base'),
-
+    url(r'^base/', views.base, name='base'),
     url(r'^login/', views.login, name='login'),
     url(r'^signup/', views.signup, name='signup'),
-    url(r'^home/', views.home, name='home'),
+
     url(r'^delete/', views.delete, name='delete'),
     url(r'^blog_page/', views.blog_page, name='blog_page'),
     url(r'^post_page/', views.post_page, name='post_page'),
+
     url(r'^postview/(?P<post_id>\d+)$', views.post_view, name='postview'),
 ];
 
